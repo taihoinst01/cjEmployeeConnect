d941acf7ee8b9631721f7247b65516fc8f1ed778
장 상련 <srjang@taihoinst.com>
2018-12-12 19:42:50 +0900

    수정

--- WebSite1/Default.aspx.cs
+++ WebSite1/Default.aspx.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-//using SSODecodeCJW;
+using SSODecodeCJW;
 
 public partial class _Default : System.Web.UI.Page
 {
@@ -14,10 +14,10 @@ public partial class _Default : System.Web.UI.Page
         string KeyStr = "CJWKEY";
         string encryptedText = "8evVae2ekt7WtC2umaHAqYVyhf2W9eNA";
 
-        //CryptoDotNet cdn = new CryptoDotNet();
-        //string PlainText = cdn.Decrypt(encryptedText, KeyStr);
+        CryptoDotNet cdn = new CryptoDotNet();
+        string PlainText = cdn.Decrypt(encryptedText, KeyStr);
 
-        Response.Write(KeyStr);
+        Response.Write(PlainText);
 
     }
 }
