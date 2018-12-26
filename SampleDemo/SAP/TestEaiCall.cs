8b9c28dfeb86f935d13fbb25a1b6671b02727ebd
장 상련 <srjang@taihoinst.com>
2018-12-26 10:59:41 +0900

    수정

--- SampleDemo/SAP/TestEaiCall.cs
+++ SampleDemo/SAP/TestEaiCall.cs
@@ -68,7 +68,7 @@ namespace SampleDemo.SAP
 
                 Debug.WriteLine("------START Send Request Message...\n" + data + "------END Send Request Message...\n");
                 HistoryLog("repData = " + data);
-                String repData = eai.sendMessage(sessionName, systemID, interfaceID, data, 5000);
+                String repData = eai.sendMessage(sessionName, systemID, interfaceID, data, 60000);
                 //JObject sapJson = new JObject();
 
                 //String repData = "bbb";
