d5f962a3ad4f0d840aa4d3e02e0546613f2aa52b
장 상련 <srjang@taihoinst.com>
2018-12-26 14:09:34 +0900

    수정

--- SampleDemo/SAP/TestEaiCall.cs
+++ SampleDemo/SAP/TestEaiCall.cs
@@ -56,8 +56,8 @@ namespace SampleDemo.SAP
                 data = data + "       'UUID': 'CJ_CHATBOT_" + System.DateTime.Now.ToString("yyyyMMddhhmmssff") + "', ";
                 //data = data + "       'OPTIONAL_1': 'CJ_SAP / CJ_BI / CJG_BI (응답시스템에 따라 선택)', ";
                 data = data + "       'OPTIONAL_1': 'CJ_SAP', ";
-                data = data + "       'OPTIONAL_2': 'CHATBOT' ";
-                data = data + "       'OPTIONAL_3': '' ";
+                data = data + "       'OPTIONAL_2': 'CHATBOT', ";
+                data = data + "       'OPTIONAL_3': '', ";
                 data = data + "       'OPTIONAL_4': '' ";
                 data = data + "     }, ";
                 data = data + "     'INPUT_DATA': { ";
